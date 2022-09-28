d1a638daf80652d1421c8bbf967cd5c711c1d944
612824735 <charlie.2.clarke@bt.com>
2022-09-28 11:38:53 +0100

    chore: added acrpull permissions

---
+++ k8s/acr.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+AKS_RESOURCE_GROUP=ECM3440
+AKS_CLUSTER_NAME=GroupAssignmentAKS
+ACR_RESOURCE_GROUP=ECM3440
+ACR_NAME=GroupAssignmentRegistry
+
+# Get the id of the service principal configured for AKS
+CLIENT_ID=$(az aks show --resource-group $AKS_RESOURCE_GROUP --name $AKS_CLUSTER_NAME --query "servicePrincipalProfile.clientId" --output tsv)
+
+# Get the ACR registry resource id
+ACR_ID=$(az acr show --name $ACR_NAME --resource-group $ACR_RESOURCE_GROUP --query "id" --output tsv)
+
+# Create role assignment
+az role assignment create --assignee $CLIENT_ID --role acrpull --scope $ACR_ID
