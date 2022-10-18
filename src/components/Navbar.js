9d1f2c5ddf6e68cacedb11f427a3f718389159e8
charlie2clarke <charliegclarke2@gmail.com>
2022-10-18 16:12:29 +0100

    fix: removing @mui/styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,29 +3,16 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import { makeStyles } from "@mui/styles";
 
-
-const useStyles = makeStyles((theme) => ({
-    appBar: {
-        top: "auto",
-        bottom: 0
-    },
-    typo: {
-        flexGrow: 1,
-        textAlign: "center"
-    }
-}));
 
 
 export default function Navbar() {
-    const classes = useStyles();
 
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
                 <Toolbar>
-                    <Typography variant="h6" className={classes.typo} component="div" sx={{ flexGrow: 1 }}>
+                    <Typography variant="h6" style={{ textAlign: "center" }} component="div" sx={{ flexGrow: 1 }}>
                         IoT Dashboard
                     </Typography>
                 </Toolbar>
